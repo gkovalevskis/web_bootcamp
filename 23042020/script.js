159f2b1122e9b4e14afd540188b76af0c76876a2
gkovalevskis <girts.kovalevskis@gmail.com>
2020-04-23 21:19:17 +0300

    23042020

---
+++ 23042020/script.js
@@ -0,0 +1,23 @@
+console.log("Hello world!");
+var name  = "Peteris";
+function thisFunction() {
+    name = "Igors";
+}
+name = "Peteris";
+// var darbojas visā js
+
+function otherFunction() {
+    let surname = "Rautmanis";
+}
+// let darbojas tikai iekš kaut kāda bloka 
+
+const c = 123;
+// constantes darbojas visā js, bet tās nevar apdeitot
+
+var array = ['1','2'];
+
+function myFunction(){
+    document.getElementById("appear").innerHTML = "TAKE A BREAK!";
+}
+
+
