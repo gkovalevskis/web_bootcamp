677f76e6a07c98831e9177ab3a1130d13a272f83
gkovalevskis <girts.kovalevskis@gmail.com>
2020-04-30 21:32:01 +0300

    30042020

---
+++ 30.04.2020/script.js
@@ -0,0 +1,28 @@
+
+let temperature = 9;
+temperature = 4;
+let person  = {
+    name: "John",
+    surname: "Vue",
+    age: 33,
+};
+let cars = ["saab", "vw", "audi"];
+function goGreen (){
+    document.getElementById("introduction").style.backgroundColor = "green";
+}
+function reloadPage(){
+    location.reload();
+}
+function goYellow (){
+    document.getElementById("footer").style.backgroundColor = "yellow";
+}
+var text = "apple";
+const record = 35;
+console.log(person.age);
+console.log(cars[2]);
+console.log(person);
+console.log(cars);
+console.log(temperature);
+console.log(text);
+console.log(record);
+// izveidot - visu tipu mainīgos
