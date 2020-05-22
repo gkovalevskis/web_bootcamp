a3d92b1deb2d99c1730dd91b7595a012e625aefb
gkovalevskis <girts.kovalevskis@gmail.com>
2020-05-22 21:38:27 +0300

    22052020

---
+++ 22052020/fromScratch/public/header.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My page!</title>
+    <link rel="stylesheet" type="text/css"href="css/my_page_style.css">
+    <link rel="stylesheet" type="text/css"href="css/navigation.css">
+</head>
